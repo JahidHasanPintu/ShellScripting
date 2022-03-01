e8fbcc5a346c1c7dc63f3343ddff7963db279c5b
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-03-01 23:42:21 +0600

    odd position number

--- oddPositionNumber.sh
+++ oddPositionNumber.sh
@@ -1,41 +1,14 @@
 #!/bin/bash
 
-# arr=(10 8 20 100 12)
-# Taking User input 
-declare -a arr
-echo "Enter size of array: "
+echo "How many digit in your number?"
+read size
+echo "enter $size digit number"
 read number
-echo "enter $number numbers: "
-
-for(( c = 0 ; c < $number ; c++))
+n=1
+for((j = 0; j<$size; j++))
 do
-  read abc_elements
-  arr[$c]="$abc_elements"
+a=`echo $number | cut -c $n`
+echo -n "$a "
+n=`expr $n + 2`
 done
-
-# printing the array 
-echo "Array in original order"
-echo ${arr[*]}
-
-# Performing Bubble sort 
-for ((i = 0; i<5; i++))
-do
-    
-    for((j = 0; j<5-i-1; j++))
-    do
-    
-        if [ ${arr[j]} -gt ${arr[$((j+1))]} ]
-        then
-            # swap
-            temp=${arr[j]}
-            arr[$j]=${arr[$((j+1))]}  
-            arr[$((j+1))]=$temp
-        fi
-    done
-done
-
-# echo "Array in sorted order :"
-# echo ${arr[*]}
-
-echo "Second Largest = ${arr[number-2]}"
-echo "Third Largest = ${arr[number-3]}"
+echo " "
