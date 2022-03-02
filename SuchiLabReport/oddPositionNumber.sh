bd345f2c1aab12827afead60f20369d57cfe4082
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-03-02 08:58:38 +0600

    lab report folder added

--- SuchiLabReport/oddPositionNumber.sh
+++ SuchiLabReport/oddPositionNumber.sh
@@ -2,9 +2,11 @@
 
 echo "How many digit in your number?"
 read size
+# taking user input 
 echo "enter $size digit number"
 read number
 n=1
+# finding odd position 
 for((j = 0; j<$size; j++))
 do
 a=`echo $number | cut -c $n`
@@ -12,3 +14,5 @@ echo -n "$a "
 n=`expr $n + 2`
 done
 echo " "
+
+# finish number 1 problem  
